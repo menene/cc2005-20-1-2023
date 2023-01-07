7976a9b5184ac4c6d6aa5ddfef9d68c960440abf
menene <efmarroquinr@gmail.com>
2023-01-07 09:56:08 -0600

    Encabezado

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cc2005-20-2023-1
+
+Repositorio de código Python para cc2005 sección 20 semestre 1 2023
---
+++ encabezado.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# ==============================================================
+# Universidad del Valle de Guatemala
+# CC2005 semestre 1 2023
+# Sección 20
+#
+# Nombre del autor
+# Número de carné
+#
+# Descripción archivo
+# Fecha de última modificación
+# ==============================================================
